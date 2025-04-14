05304042aeaa7bedb0c69771566943f46b7bad83
Jesper <jesper.l.richert@gmail.com>
2025-04-14 23:18:48 +0200

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -40,7 +40,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/DisBotDevelopment/DisBot-Docs/blob/master/docs/',
+            'https://github.com/DisBotDevelopment/DisBot-Docs/blob/master/',
         },
         // blog: {
         //   showReadingTime: true,
